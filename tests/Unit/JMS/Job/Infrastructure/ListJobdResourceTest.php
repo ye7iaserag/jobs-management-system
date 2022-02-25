415d4a478f5890e5ab0248fbc4e1da8113385ace
Yehia Serag <yehia.serag@softxpert.com>
2022-02-26 01:50:44 +0200

    Tests for resources

---
+++ tests/Unit/JMS/Job/Infrastructure/ListJobdResourceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\JMS\Job\Infrastructure;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Request;
+use JMS\Job\Application\JobResponse;
+use JMS\Job\Application\JobsResponse;
+use JMS\Job\Infrastructure\Http\Resources\ListJobsResource;
+use Tests\TestCase;
+
+final class ListJobdResourceTest extends TestCase
+{
+    use WithFaker;
+
+    function test_list_jobs_resource()
+    {
+        $resource = new ListJobsResource(new JobsResponse([new JobResponse($this->faker->uuid(), $this->faker->name(), $this->faker->name(), $this->faker->uuid())]));
+
+        $array = $resource->toArray($this->app->make(Request::class));
+
+        $this->assertIsArray($array);
+        $this->assertArrayHasKey('id', $array[0]);
+        $this->assertArrayHasKey('title', $array[0]);
+        $this->assertArrayHasKey('description', $array[0]);
+        $this->assertArrayHasKey('ownerId', $array[0]);
+    }
+
+}
